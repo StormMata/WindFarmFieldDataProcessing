81c93e20d5809c821740fea3dd30fd9f421dcc52
StormMata <storm.mata097@gmail.com>
2022-05-17 23:45:42 -0400

    17-May-22; 23:45

--- LoadFullData.m
+++ LoadFullData.m
@@ -5,7 +5,7 @@ function [data] = LoadFullData()
 addpath('/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/Courses/Research/Field Data Power Modeling/functions/figplots');
 addpath('/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/Courses/Research/Field Data Power Modeling/functions');
 
-folderCoord = '/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/Courses/Research/Conditional Averages/WindFarmFieldDataProcessing/';
+folderCoord = '/Users/stormmata/Library/Mobile Documents/com~apple~CloudDocs/Courses/Research/Data/';
 
 data = load(strcat(folderCoord, 'AllData_NP_corrected.mat')); 
 data = data.AllData;
